05e425fecb6c004f730d46006fcbadc2b50b6520
Emiliano <emialmasia@gmail.com>
2026-04-03 19:53:05 -0300

    Update railway-setup.sh
    echo '{"socketio_port": 9000}' > /home/frappe/bench/sites/common_site_config.json

--- railway/railway-setup.sh
+++ railway/railway-setup.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 set -e
 
-# -> Run entrypoint
-# somehow when specify custom cmd in railway,
-# it doesn't run entrypoint first, so we need to run it here.
 sudo /usr/local/bin/railway-entrypoint.sh
 
-echo "-> Create empty common site config"
-echo "{}" > /home/frappe/bench/sites/common_site_config.json
+echo "-> Create common site config"
+echo '{"socketio_port": 9000}' > /home/frappe/bench/sites/common_site_config.json
 
 echo "-> Create new site with ERPNext"
-bench new-site ${RFP_DOMAIN_NAME} --admin-password ${RFP_SITE_ADMIN_PASSWORD} --no-mariadb-socket --db-root-password ${RFP_DB_ROOT_PASSWORD} --install-app erpnext
+bench new-site ${RFP_DOMAIN_NAME} \
+  --admin-password ${RFP_SITE_ADMIN_PASSWORD} \
+  --no-mariadb-socket \
+  --db-root-password ${RFP_DB_ROOT_PASSWORD} \
+  --install-app erpnext
+
+echo "-> Install HRMS"
+bench --site ${RFP_DOMAIN_NAME} install-app hrms
+
+echo "-> Migrate site"
+bench --site ${RFP_DOMAIN_NAME} migrate
+
 bench use ${RFP_DOMAIN_NAME}
 
 echo "-> Enable scheduler"
